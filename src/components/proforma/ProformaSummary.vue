eb5b23abd25bdf56a5e00b3b7d9a7beea30e654b
name <ebammo20@gmail.com>
2020-04-14 17:05:08 +0430

    Adding Proforma summary and specs for dashboard.

---
+++ src/components/proforma/ProformaSummary.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout justify-space-around row>
+        <v-flex xs12 md10>
+          <v-card>
+            <v-card-title>پیش فاکتور</v-card-title>
+            <v-card-text>
+              <v-data-table
+                :headers="proformasHeader"
+                :items="proformas"
+                :expanded="proformaRowExpanded"
+                show-expand
+                single-expand
+                @item-expanded="proformaExpanded">
+                <template v-slot:expanded-item="{headers}">
+                  <td :colspan="headers.length">
+                    <v-data-table
+                      :headers="specHeaders"
+                      :items="specs"
+                    @click:row="findProformas">
+                    </v-data-table>
+                  </td>
+                </template>
+              </v-data-table>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-dialog v-model="specProformasDialog" width="900px">
+      <spec-proformas :specProformas="specProformas"/>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+  import SpecProformas from "./SpecProformas";
+
+  export default {
+    data(){
+      return {
+        name: "ProformaSummary",
+        proformaRowExpanded: [],
+        specProformasDialog: false,
+        proformasHeader: [
+          {value: "proformaNumber", text: "پیش فاکتور"},
+          {value: "number", text: "شماره"},
+          {value: "customerName", text: "مشتری"},
+          {value: "date", text: "تاریخ"},
+          {value: "expert", text: "کارشناس"},
+        ],
+        proformas: [
+          {id: 1, proformaNumber: 980025, number: 990041, customerName: "شرکت زرین ذرت شاهرود", date: "1399-01-23", expert: "ظریف"},
+          {id: 2, proformaNumber: 980026, number: 990042, customerName: "آرمان گسترنوین کنارک", date: "1399-01-23", expert: "علوی"},
+          {id: 3, proformaNumber: 980027, number: 990043, customerName: "صنایع بسته بندی فرآورده های شیری پگاه", date: "1399-01-23", expert: "علوی"},
+          {id: 4, proformaNumber: 980060, number: 990045, customerName: "پویاموتور سپاهان", date: "1399-01-23", expert: "ظریف"},
+        ],
+        specHeaders: [
+          {value: "qty", text: "تعداد"},
+          {value: "kw", text: "کیلووات"},
+          {value: "voltage", text: "ولتاژ"},
+          {value: "rpm", text: "سرعت"},
+          {value: "price", text: "قیمت واحد"},
+        ],
+        specs: [
+          {qty: 5, kw: 132, voltage: 380, rpm: 1500, price: 45500000},
+          {qty: 2, kw: 160, voltage: 380, rpm: 1500, price: 49800000},
+          {qty: 3, kw: 75, voltage: 380, rpm: 1000, price: 58500000},
+          {qty: 1, kw: 90, voltage: 380, rpm: 3000, price: 987500000},
+        ],
+        specProformas: [
+          {number: 981351, customer: "پتروشیمی پردیس", qty: 10, cost: 25600000, price: 27500000, percentage: 12, date: "1399-01-12"},
+          {number: 985241, customer: "پتروشیمی مارون", qty: 5, cost: 25600000, price: 27700000, percentage: 12.5, date: "1399-01-13"},
+          {number: 986548, customer: "پتروشیمی تخت جمشید", qty: 1, cost: 25600000, price: 28500000, percentage: 13, date: "1399-01-18"},
+          {number: 982564, customer: "هوایار", qty: 3, cost: 25600000, price: 29800000, percentage: 15, date: "1399-01-20"},
+          {number: 983548, customer: "پتروشیمی پردیس", qty: 9, cost: 25600000, price: 27500000, percentage: 16, date: "1399-01-23"},
+        ]
+      }
+    },
+    methods: {
+      proformaExpanded(value){
+        console.log(value.item, this.proformaRowExpanded)
+        if(this.proformaRowExpanded.includes(value.item)){
+          this.proformaRowExpanded.pop(value.item);
+        }else {
+          this.proformaRowExpanded = [];
+          this.proformaRowExpanded.push(value.item);
+        }
+      },
+      findProformas(spec){
+        console.log(spec)
+        this.specProformasDialog = true;
+      }
+    },
+    components: {
+      SpecProformas
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/proforma/SpecProformas.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>
+        {{type}}های صادره برای این محصول
+      </v-card-title>
+      <v-card-text>
+        <v-data-table
+          dense
+          :items="specProformas"
+          :headers="specProformaHeaders">
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        name: "SpecProformas",
+        type: "پیش فاکتور",
+        specProformaHeaders: [
+          {value: "number", text: "پیش فاکتور"},
+          {value: "customer", text: "مشتری"},
+          {value: "qty", text: "تعداد"},
+          {value: "cost", text: "قیمت تمام شده"},
+          {value: "price", text: "قیمت فروش"},
+          {value: "percentage", text: "درصد سود"},
+          {value: "date", text: "تاریخ"},
+        ],
+      }
+    },
+    beforeCreate() {
+    },
+    created() {
+      if (this.config && this.config.type){
+        let type = this.specProformaHeaders.filter(e => e.value === "number");
+        let index = this.specProformaHeaders.indexOf(type[0]);
+        this.type = this.config.type;
+        this.specProformaHeaders[index].text = this.config.type;
+      }
+    },
+    mounted(){
+    },
+    beforeMount() {
+    },
+    props: [
+      "specProformas",
+      "config"
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
